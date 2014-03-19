da50418346546af89e7586b83fb8d08ef00be34c
christianxrow <christian@xrow.de>
2014-03-19 13:37:53 +0100

    fixed template loading for module test/html

--- modules/test/html.php
+++ modules/test/html.php
@@ -4,6 +4,6 @@ $Module =& $Params['Module'];
 $tpl = eZTemplate::factory();
 $Result = array();
 
-$Result['content'] = $tpl->fetch( 'design:ezadmin/test/html.tpl' );
+$Result['content'] = $tpl->fetch( 'design:xrowadmin/test/html.tpl' );
 $Result['path'] = array( array( 'url' => false,
                                 'text' => 'Test HTML' ) );
