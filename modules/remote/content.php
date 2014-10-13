b364e62ad03e50fbe094d33d0926dbaea3d2a1d7
Christian H <christian@xrow.de>
2014-10-13 15:12:09 +0200

    regex update for module remote/content/full

--- modules/remote/content.php
+++ modules/remote/content.php
@@ -68,7 +68,7 @@ if( isset( $namedParameters['Type'] ) )
             }
             if( $remote_content )
             {
-                $remote_content = preg_replace( '#(href|src)="([^:"]*)("|(?:(?:%20|\s|\+)[^"]*"))#', '$1="' . $remote_url['host'] . '$2$3', $remote_content );
+                $remote_content = preg_replace( '#(href|src)="(?!(|http:|https:)//)([^:"]*)("|(?:(?:%20|\s|\+)[^"]*"))#', '$1="' . $remote_url['host'] . '$2$3"', $remote_content );
                 $remote_content = preg_replace( '#url\((?!\s*[\'"]?(?:https?:)?//)\s*([\'"])?#', "url($1{$remote_url['host']}", $remote_content );
                 switch ( $namedParameters['Type'] )
                 {
