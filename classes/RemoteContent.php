e019fa9390b6e09559124c3c12b120add0f32657
xrow <bjoern@xrow.de>
2014-10-15 07:06:53 +0200

    - classes/RemoteContent.php

---
+++ classes/RemoteContent.php
@@ -0,0 +1,18 @@
+<?php
+
+class RemoteContent
+{
+    public const DEFAULT_MARKER = "<!--CONTENT-->";
+    static public function getMarker()
+    {
+        $remote_ini = eZINI::instance( 'remotecontent.ini' );
+        if( $remote_ini->hasVariable( 'Settings', 'ContentMarker' ) )
+        {
+            return $remote_ini->variable( 'Settings', 'ContentMarker' );
+        }
+        else
+        {
+            return self::DEFAULT_MARKER;
+        }
+    }
+}
